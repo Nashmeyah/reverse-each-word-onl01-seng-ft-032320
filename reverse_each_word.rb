6dbeecdcb18e24f30c90dfc3268b3b2a6011b3d2
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-03-26 01:02:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   new_string = ""
   sentence.split(' ').each do |word|
-    new_string += word.reverse + " "
+    new_string +=  " " + word.reverse 
   end
   new_string
 end
