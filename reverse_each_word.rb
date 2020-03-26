89eb1d49630c73f223152f57f4e6bd8a6a62f61c
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-03-26 01:21:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   new_arr = []
   sentence.split(' ').each do |word|
-    new_string << word.reverse
+    new_arr << word.reverse
   end
   return new_arr.join(' ')
 end
